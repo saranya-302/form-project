41229efb9506eb37134dc303e3e3d143b57e3a1d
saranya-302 <saranyag3026@gmail.com>
2024-09-09 21:15:47 +0530

    App.jsx and App.css files added

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Main from './Global-project/Main';
+function App() {
+  return (
+      <div>
+      <Main/>
+      </div>
+      );
+}
+
+export default App;
